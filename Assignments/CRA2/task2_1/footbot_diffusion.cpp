fa7793b1d25a42ad31d2a53d5bbf2e5fe1193dea
Trushar2411 <trushar.ghanekar2411@gmail.com>
2025-05-13 16:07:01 +0200

    update task

--- Assignments/CRA2/task2_1/footbot_diffusion.cpp
+++ Assignments/CRA2/task2_1/footbot_diffusion.cpp
@@ -1,7 +1,9 @@
 /* Include the controller definition */
 #include "footbot_diffusion.h"
+
 /* Function definitions for XML parsing */
 #include <argos3/core/utility/configuration/argos_configuration.h>
+
 /* 2D vector definition */
 #include <argos3/core/utility/math/vector2.h>
 
@@ -13,7 +15,7 @@ CFootBotDiffusion::CFootBotDiffusion() :
    m_pcProximity(NULL),
    m_cAlpha(10.0f),
    m_fDelta(0.5f),
-   m_fWheelVelocity(2.5f),
+   m_fWheelVelocity(1.5f),
    m_cGoStraightAngleRange(-ToRadians(m_cAlpha),
                            ToRadians(m_cAlpha)) {}
 
@@ -23,34 +25,12 @@ CFootBotDiffusion::CFootBotDiffusion() :
 void CFootBotDiffusion::Init(TConfigurationNode& t_node) {
    /*
     * Get sensor/actuator handles
-    *
-    * The passed string (ex. "differential_steering") corresponds to the
-    * XML tag of the device whose handle we want to have. For a list of
-    * allowed values, type at the command prompt:
-    *
-    * $ argos3 -q actuators
-    *
-    * to have a list of all the possible actuators, or
-    *
-    * $ argos3 -q sensors
-    *
-    * to have a list of all the possible sensors.
-    *
-    * NOTE: ARGoS creates and initializes actuators and sensors
-    * internally, on the basis of the lists provided the configuration
-    * file at the <controllers><footbot_diffusion><actuators> and
-    * <controllers><footbot_diffusion><sensors> sections. If you forgot to
-    * list a device in the XML and then you request it here, an error
-    * occurs.
     */
    m_pcWheels    = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
-   m_pcProximity = GetSensor  <CCI_FootBotProximitySensor      >("footbot_proximity"    );
+   m_pcProximity = GetSensor  <CCI_FootBotProximitySensor      >("footbot_proximity");
+
    /*
-    * Parse the configuration file
-    *
-    * The user defines this part. Here, the algorithm accepts three
-    * parameters and it's nice to put them in the config file so we don't
-    * have to recompile if we want to try other settings.
+    * Parse configuration file attributes
     */
    GetNodeAttributeOrDefault(t_node, "alpha", m_cAlpha, m_cAlpha);
    m_cGoStraightAngleRange.Set(-ToRadians(m_cAlpha), ToRadians(m_cAlpha));
@@ -62,31 +42,25 @@ void CFootBotDiffusion::Init(TConfigurationNode& t_node) {
 /****************************************/
 
 void CFootBotDiffusion::ControlStep() {
-   /* Get readings from proximity sensor */
+   /* Get proximity sensor readings */
    const CCI_FootBotProximitySensor::TReadings& tProxReads = m_pcProximity->GetReadings();
-   /* Sum them together */
-   CVector2 cAccumulator;
-   for(size_t i = 0; i < tProxReads.size(); ++i) {
-      cAccumulator += CVector2(tProxReads[i].Value, tProxReads[i].Angle);
+
+   /* Check if any reading exceeds the threshold */
+   bool bObstacleDetected = false;
+   for(const auto& tReading : tProxReads) {
+      if(tReading.Value > m_fDelta) {
+         bObstacleDetected = true;
+         break;
+      }
    }
-   cAccumulator /= tProxReads.size();
-   /* If the angle of the vector is small enough and the closest obstacle
-    * is far enough, continue going straight, otherwise curve a little
-    */
-   CRadians cAngle = cAccumulator.Angle();
-   if(m_cGoStraightAngleRange.WithinMinBoundIncludedMaxBoundIncluded(cAngle) &&
-      cAccumulator.Length() < m_fDelta ) {
-      /* Go straight */
-      m_pcWheels->SetLinearVelocity(m_fWheelVelocity, m_fWheelVelocity);
+
+   if(bObstacleDetected) {
+      /* Stop if anything is detected */
+      m_pcWheels->SetLinearVelocity(0.0f, 0.0f);
    }
    else {
-      /* Turn, depending on the sign of the angle */
-      if(cAngle.GetValue() > 0.0f) {
-         m_pcWheels->SetLinearVelocity(m_fWheelVelocity, 0.0f);
-      }
-      else {
-         m_pcWheels->SetLinearVelocity(0.0f, m_fWheelVelocity);
-      }
+      /* Otherwise, move forward */
+      m_pcWheels->SetLinearVelocity(m_fWheelVelocity, m_fWheelVelocity);
    }
 }
 
@@ -94,13 +68,6 @@ void CFootBotDiffusion::ControlStep() {
 /****************************************/
 
 /*
- * This statement notifies ARGoS of the existence of the controller.
- * It binds the class passed as first argument to the string passed as
- * second argument.
- * The string is then usable in the configuration file to refer to this
- * controller.
- * When ARGoS reads that string in the configuration file, it knows which
- * controller class to instantiate.
- * See also the configuration files for an example of how this is used.
+ * Register the controller with ARGoS
  */
 REGISTER_CONTROLLER(CFootBotDiffusion, "footbot_diffusion_controller")
